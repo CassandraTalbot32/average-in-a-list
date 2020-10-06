fb713817734fe85c20f3040e128199919d72bbf4
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2020-10-06 12:00:10 +0100

    Add files via upload

---
+++ average in a list.py
@@ -0,0 +1,7 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> list = [2.1, 4.5, 5.6, 7.8, 9.1, 10, 123]
+>>> average = sum(list) / len(list)
+>>> print("The average of this list is " + str(round(average, 2)))
+The average of this list is 23.16
+>>> 
